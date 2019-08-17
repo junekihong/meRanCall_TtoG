7fb07a47798de76173ef31d2a19b2321407813b9
Juneki Hong <hongju@oregonstate.edu>
2019-08-16 19:00:22 -0700

    Example script for partha and yogesh

---
+++ run_partha-yogesh_data.sh
@@ -0,0 +1,11 @@
+time python3 src/mapping.py --gtf Refseq_mm10.gff3 --fasta Transcriptome_to_genomic/mm10.refSeqRNA-trna.fa --input Transcriptome_to_genomic/J1-1_tRNA_FDR10_meRanCallResult.txt --output output/Partha/J1-1.genomic-coord.txt
+time python3 src/mapping.py --gtf Refseq_mm10.gff3 --fasta Transcriptome_to_genomic/mm10.refSeqRNA-trna.fa --input Transcriptome_to_genomic/J1-2_tRNA_FDR10_meRanCallResult.txt --output output/Partha/J1-2.genomic-coord.txt
+time python3 src/mapping.py --gtf Refseq_mm10.gff3 --fasta Transcriptome_to_genomic/mm10.refSeqRNA-trna.fa --input Transcriptome_to_genomic/J1-3_tRNA_FDR10_meRanCallResult.txt --output output/Partha/J1-3.genomic-coord.txt
+
+time python3 src/mapping.py --gtf Refseq_mm10.gff3 --fasta Transcriptome_to_genomic/mm10.refSeqRNA-trna.fa --input Transcriptome_to_genomic/Nsun2-D2-1_tRNA_FDR10_meRanCallResult.txt --output output/Partha/Nsun2-D2-1.genomic-coord.txt
+time python3 src/mapping.py --gtf Refseq_mm10.gff3 --fasta Transcriptome_to_genomic/mm10.refSeqRNA-trna.fa --input Transcriptome_to_genomic/Nsun2-D2-2_tRNA_FDR10_meRanCallResult.txt --output output/Partha/Nsun2-D2-2.genomic-coord.txt
+time python3 src/mapping.py --gtf Refseq_mm10.gff3 --fasta Transcriptome_to_genomic/mm10.refSeqRNA-trna.fa --input Transcriptome_to_genomic/Nsun2-D2-3_tRNA_FDR10_meRanCallResult.txt --output output/Partha/Nsun2-D2-3.genomic-coord.txt
+
+time python3 src/mapping.py --gtf Refseq_mm10.gff3 --fasta Transcriptome_to_genomic/mm10.refSeqRNA-trna.fa --input Transcriptome_to_genomic/Nsun2-D5-1_tRNA_FDR10_meRanCallResult.txt --output output/Partha/Nsun2-D5-1.genomic-coord.txt
+time python3 src/mapping.py --gtf Refseq_mm10.gff3 --fasta Transcriptome_to_genomic/mm10.refSeqRNA-trna.fa --input Transcriptome_to_genomic/Nsun2-D5-2_tRNA_FDR10_meRanCallResult.txt --output output/Partha/Nsun2-D5-2.genomic-coord.txt
+time python3 src/mapping.py --gtf Refseq_mm10.gff3 --fasta Transcriptome_to_genomic/mm10.refSeqRNA-trna.fa --input Transcriptome_to_genomic/Nsun2-D5-3_tRNA_FDR10_meRanCallResult.txt --output output/Partha/Nsun2-D5-3.genomic-coord.txt
